6bbc186e73f2186df067740f2e9c3c159565e025
khkang <khkang@promedius.ai>
2021-05-13 16:25:57 +0900

    sample scenario

---
+++ lib/result.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text('You did it!'),
+        ],
+      ),
+    );
+  }
+}
